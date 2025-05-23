34fc06b5f5faa3b0115bfb26ac79f245877c4432
cnetsec <86935257+cnetsec@users.noreply.github.com>
2025-05-22 22:41:00 -0300

    Delete run.py

--- run.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-if __name__ == '__main__':
-    # Running in debug mode (not recommended for production, can expose internals)
-    app.run(host='0.0.0.0', port=5000, debug=True)
